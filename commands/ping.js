a2f4725e70c72187fbdc68e28bdd9366b152ad7c
Cylis Shults <43319239+CyPlayz@users.noreply.github.com>
2022-12-14 13:10:04 +0000

    Made Ping Command

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,10 @@
 const {CommandType} = require("wokcommands");
 module.exports = {
-  description: 'Pong',
+  description: 'Bot Latency',
   slash: true,
   testOnly: true,
 
-  callback: ({interaction}) => {
-    interaction.reply('Pong')
+  callback: ({client, message, interaction }) => {
+      interaction.reply(`Bot Ping is ${Date.now() - interaction.createdTimestamp}ms. API Ping is ${Math.round(client.ws.ping)}ms`);
   },
 };
